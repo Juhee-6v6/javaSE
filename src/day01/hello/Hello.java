9653c875f3817f0e4bb8bfc1835b16236bafa5db
ITSC <ITSC@DESKTOP-ONE4R81>
2022-11-14 19:39:27 +0900

    첫 커밋

---
+++ src/day01/hello/Hello.java
@@ -0,0 +1,9 @@
+package day01.hello;
+
+public class Hello {
+	/*main 치고 ctrl + 스페이스, 한번 더 스페이스, syso 치고 똑같이 컨트롤 + 스페이스*/
+	public static void main(String[] args) {
+		System.out.println("Hello World");	//실행 단축키 F11
+		
+	}
+}
