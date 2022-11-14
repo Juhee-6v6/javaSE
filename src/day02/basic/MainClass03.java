140dc4d379f16f7e1dd0af7f77bb4e9e264529bc
ITSC <ITSC@DESKTOP-ONE4R81>
2022-11-14 21:48:37 +0900

    2022-11-14 수업 마무리

--- src/day02/basic/MainClass03.java
+++ src/day02/basic/MainClass03.java
@@ -2,10 +2,10 @@ package day02.basic;
 
 /*
 [[기본 가료형]]
-		2. 문자형(char)
-		- 선언하는 방법
-			char 변수명;
-		- 전세계에서 쓰이는 문자 형태 대부분 표현가능.
+	2. 문자형(char)
+	- 선언하는 방법
+		char 변수명;
+	- 전세계에서 쓰이는 문자 형태 대부분 표현가능.
 */
 public class MainClass03 {
 	public static void main(String[] args) {
@@ -22,6 +22,8 @@ public class MainClass03 {
 		int num2 = 110;
 		char ch4 = (char)num2; // int 형을 char 형으로 강제 형변환
 		System.out.println(ch4);
+		
+		// char는 2byte, int는 4byte
 	}
 
 }
